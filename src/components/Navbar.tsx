04aa87ac5be859d2d597f60ff4402a841fc71123
Taha Zoabi <zoabi03@gmail.com>
2024-10-09 12:26:10 +0300

    nav bar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,42 +2,69 @@ import logo from "../../public/logo.png";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import MenuIcon from "@mui/icons-material/Menu";
+
 function Navbar() {
   return (
-    <div className=" flex justify-around items-center border-2 p-4 mb-7">
-      <div className=" flex items-center">
+    <div className="flex justify-between items-center p-4 mb-7">
+      {/* Logo */}
+      <div className="flex items-center">
         <img
-          className=" h-20 rounded-full cursor-pointer"
+          className="h-16 rounded-full cursor-pointer"
           src={logo}
           alt="logo"
         />
       </div>
-      <nav className="w-4/6">
-        <ul className=" flex gap-10 justify-center ">
+
+      <nav className="hidden sm:block">
+        <ul className="flex gap-6 justify-center">
           <li>
-            <a href="#home">Home</a>
+            <a
+              className="hover:bg-gradient-to-r from-[#DF8908] to-[#B415FF] hover:bg-clip-text hover:text-transparent text-xl"
+              href="#home"
+            >
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a
+              className="hover:bg-gradient-to-r from-[#DF8908] to-[#B415FF] hover:bg-clip-text hover:text-transparent text-xl"
+              href="#about"
+            >
+              About
+            </a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a
+              className="hover:bg-gradient-to-r from-[#DF8908] to-[#B415FF] hover:bg-clip-text hover:text-transparent text-xl"
+              href="#projects"
+            >
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a
+              className="hover:bg-gradient-to-r from-[#DF8908] to-[#B415FF] hover:bg-clip-text hover:text-transparent text-xl"
+              href="#contact"
+            >
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
-      <div className="flex gap-4">
+
+      {/* Buttons (Light/Dark mode and Connect) */}
+      <div className="flex items-center gap-4">
+        <button className="md:hidden">
+          <MenuIcon />
+        </button>
         <button>
           <WbSunnyIcon />
         </button>
-        <button>
+        {/* <button>
           <DarkModeIcon />
-        </button>
-
-        <button>
-          <MenuIcon />
+        </button> */}
+        <button className="bg-custom-gradient rounded-[50px] px-6 py-3 text-md hidden md:block">
+          Connect With Me
         </button>
       </div>
     </div>
