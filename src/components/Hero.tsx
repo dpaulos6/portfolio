b8a82c6527a873ebbe23e27c08c27c2f7c75113c
Taha Zoabi <zoabi03@gmail.com>
2024-10-09 16:54:06 +0300

    hero responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,27 +6,35 @@ function Hero() {
       <img
         src={profileImg}
         alt="pesonal-picture"
-        className="rounded-full w-[240px] mt-[30px]"
+        className=" w-[240px] mt-[30px]
+        "
       />
-      <h1 className="text-[90px] text-center w-[70%] font-semibold">
+      <h1
+        className=" text-3xl sm:text-4xl md:text-5xl lg:text-6xl
+                 w-[80%] mt-12 text-center md:w-[70%] lg:w-[60%]
+                 font-semibold"
+      >
         <span className=" bg-clip-text text-transparent bg-gradient-to-r from-[#DF8908] to-[#B415FF]">
           I'm Taha Zoabi,
         </span>{" "}
-        Software Engineer Student.
+        Software Engineering Student.
       </h1>
-      <p className="w-[50%] text-center text-2xl leading-10">
+      <p
+        className=" w-full md:w-[90%] text-md  md:text-lg leading-6 text-center
+                 sm:leading-10"
+      >
         Aspiring software engineer dedicated to learning and mastering
         innovative technologies.
       </p>
       <div className="flex gap-6 items-center mb-12">
         <Button
-          classes="bg-custom-gradient rounded-[50px] px-10 py-5 text-md
-          hover:border-2
+          classes="bg-custom-gradient rounded-[50px] px-4 py-3 text-md
+          hover:border-2 sm:px-10 py-5
         "
         >
           Connect With Me
         </Button>
-        <Button classes="border-2 rounded-[50px] px-10 py-5 text-md  hover:border-[#B415FF]">
+        <Button classes="border-2 rounded-[50px] px-4 py-3 text-md  hover:border-[#B415FF] sm:px-10 py-5 ">
           Download Resume
         </Button>
       </div>
