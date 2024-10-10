6301e4f322789a1246788e748abb3e4a440387a2
Taha Zoabi <zoabi03@gmail.com>
2024-10-10 14:47:01 +0300

    light/dark mode adjusted

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Menu, Moon, Sun } from 'lucide-react'
 import { useTheme } from '../lib/theme-provider.tsx'
 import { Button } from './ui/Button.tsx'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger
-} from './ui/DropDownMenu.tsx'
+import { DropdownMenu, DropdownMenuTrigger } from './ui/DropDownMenu.tsx'
 
 function Header() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
   return (
     <div className="mb-7 flex items-center justify-between p-4 lg:w-[90%] lg:justify-evenly">
       {/* Logo */}
@@ -65,29 +59,21 @@ function Header() {
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button
+              onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
               variant="outline"
               size="icon"
             >
-              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              {theme === 'dark' ?
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+              : <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              }
               <span className="sr-only">Toggle theme</span>
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem onClick={() => setTheme('light')}>
-              Light
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme('dark')}>
-              Dark
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme('system')}>
-              System
-            </DropdownMenuItem>
-          </DropdownMenuContent>
         </DropdownMenu>
 
         <DropdownMenu>
-          <Menu />
+          <Menu className={'md:hidden'} />
         </DropdownMenu>
       </div>
     </div>
