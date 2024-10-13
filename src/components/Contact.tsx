103bce28468cf65b9c3bd230bce6a87c8a6127f3
Taha Zoabi <zoabi03@gmail.com>
2024-10-13 16:53:23 +0300

    added form validation with react-hook-form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,45 +1,13 @@
 import Title from './ui/Title.tsx'
-import Button from './ui/Button.tsx'
 import Footer from './ui/Footer.tsx'
 import { Mail, Phone, MapPin } from 'lucide-react'
-import { Input } from './ui/Input.tsx'
-import { Textarea } from './ui/TextArea.tsx'
-import { Alert, AlertDescription, AlertTitle } from './ui/Alert.tsx'
-import React, { useState } from 'react'
+import Form from './Form.tsx'
 
 function Contact() {
-  const [showAlert, setShowAlert] = useState(false)
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    const formData = new FormData(event.target)
-
-    formData.append('access_key', 'd2855961-b6a6-43a9-b17b-3dbaf56f446a')
-
-    const object = Object.fromEntries(formData)
-    const json = JSON.stringify(object)
-
-    const res = await fetch('https://api.web3forms.com/submit', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
-      },
-      body: json
-    }).then((res) => res.json())
-
-    if (res.success) {
-      console.log('Success', res)
-    }
-    setShowAlert((prevState) => !prevState)
-    setTimeout(() => setShowAlert((prevState) => !prevState), 3000)
-  }
-
   return (
     <section>
       <Title>Contact Me</Title>
+      {/* Contact Description */}
       <div
         id={'contact me'}
         className="flex flex-col items-center justify-center p-3 md:flex-row"
@@ -69,55 +37,9 @@ function Contact() {
               </p>
             </div>
           </div>
+          {/*Form*/}
           <div className="flex w-full flex-col lg:w-2/4">
-            <form
-              onSubmit={handleSubmit}
-              className="flex flex-col items-center gap-5 p-4"
-            >
-              <div className="flex w-full flex-col gap-4">
-                <label className="flex flex-col gap-2 shadow-md">
-                  Your Name
-                  <Input
-                    type={'text'}
-                    name={'name'}
-                    placeholder={'Enter your name'}
-                    required
-                  />
-                </label>
-                <label className="flex flex-col gap-2 shadow-md">
-                  Your Email
-                  <Input
-                    type={'email'}
-                    name={'email'}
-                    placeholder={'Enter your email'}
-                    required
-                  />
-                </label>
-                <label className="flex flex-col gap-2 shadow-md">
-                  Write your message here
-                  <Textarea
-                    rows={8}
-                    name={'message'}
-                    placeholder={'Type your message here.'}
-                    required
-                  />
-                </label>
-              </div>
-
-              <Button className="w-full rounded-lg bg-custom-gradient px-5 py-4 text-sm font-semibold ring-2 ring-inset ring-transparent transition-all hover:ring-foreground sm:px-10 lg:w-1/4">
-                Send
-              </Button>
-              {showAlert && (
-                <Alert
-                  className={
-                    'fixed left-1/2 top-5 z-10 -translate-x-1/2 transform'
-                  }
-                >
-                  <AlertTitle>Notification!</AlertTitle>
-                  <AlertDescription>Email Sent Successfully!</AlertDescription>
-                </Alert>
-              )}
-            </form>
+            <Form />
           </div>
         </div>
       </div>
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,8 +3,16 @@ import React, { ReactNode } from 'react'
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: ReactNode
   className?: string
+  disabled?: boolean
 }
 
-export default function Button({ children, className }: ButtonProps) {
-  return <button className={className}>{children}</button>
+export default function Button({ children, className, disabled }: ButtonProps) {
+  return (
+    <button
+      disabled={disabled}
+      className={className}
+    >
+      {children}
+    </button>
+  )
 }
