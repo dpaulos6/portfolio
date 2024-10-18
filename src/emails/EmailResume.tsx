34005f6b9ce6e43fce67d34d194c1b8877c50aab
Taha Zoabi <zoabi03@gmail.com>
2024-10-18 11:46:52 +0300

    added email content and resume download link

--- src/emails/EmailResume.tsx
+++ src/emails/EmailResume.tsx
@@ -1,14 +1,39 @@
-import { Button, Html } from '@react-email/components'
+import {
+  Html,
+  Head,
+  Body,
+  Text,
+  Button,
+  Container
+} from '@react-email/components'
 
-export default function Email() {
+const ResumeEmail = () => {
   return (
     <Html>
-      <Button
-        href="https://example.com"
-        style={{ background: '#000', color: '#fff', padding: '12px 20px' }}
-      >
-        Click me
-      </Button>
+      <Head />
+      <Body className="bg-gray-100 p-6 font-serif text-gray-400">
+        <Container className="mx-auto max-w-lg rounded-lg bg-white p-8 shadow-md">
+          <Text className="mb-4 text-xl font-bold">Hello!</Text>
+          <Text className="mb-6 text-base">
+            I’m thrilled to connect with you! I’ve included my resume for your
+            review. Click the button below to download it. I’m looking forward
+            to discussing how I can add value to your organization!
+          </Text>
+          <Button
+            className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+            href="https://drive.google.com/file/d/1VQaapob6UVxHO7bnj31DaUL-CG2tEgpm/view?usp=sharing"
+          >
+            Download My Resume
+          </Button>
+          <Text className="mt-6 text-sm text-gray-600">
+            Best regards,
+            <br />
+            Taha Zoabi
+          </Text>
+        </Container>
+      </Body>
     </Html>
   )
 }
+
+export default ResumeEmail
